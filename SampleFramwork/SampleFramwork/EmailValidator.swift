fb572b60c65515c0e9f54349a13b5d2340aff4cf
Rahul Sharma <rahul.s11@tataelxsi.co.in>
2024-01-12 15:12:33 +0530

    add framwork

---
+++ SampleFramwork/SampleFramwork/EmailValidator.swift
@@ -0,0 +1,20 @@
+//
+//  EmailValidator.swift
+//  SampleFramwork
+//
+//  Created by Rahul Sharma TE on 12/01/24.
+//
+
+import Foundation
+
+public struct EmailValidator {
+    
+   public static func emailValidation(_ value: String) -> Bool {
+        print("check email validation here")
+        return true
+    }
+    
+   public static func sayHello(){
+        print("hello how are u doing ?")
+    }
+}
---
+++ SampleFramwork/SampleFramwork/SampleFramwork.h
@@ -0,0 +1,18 @@
+//
+//  SampleFramwork.h
+//  SampleFramwork
+//
+//  Created by Rahul Sharma TE on 12/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFramwork.
+FOUNDATION_EXPORT double SampleFramworkVersionNumber;
+
+//! Project version string for SampleFramwork.
+FOUNDATION_EXPORT const unsigned char SampleFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramwork/PublicHeader.h>
+
+
